1d50725356c6302858c7389e4fa3caf167cd0703
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-01 23:47:49 -0800

    ff

---
+++ script.js
@@ -0,0 +1,5 @@
+// forEach isn't returning something in javaScript but map method is returning/generating new array.
+const fruits = ["banana", "apple", "peach", "mango", "grapes"];
+fruits.forEach((x) => {
+  return x;
+});
