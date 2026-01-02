c4cf1b68bfea71d4314a77c39cdd884a60cd32b2
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-02 00:48:49 -0800

    updated the JavaScript.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // forEach isn't returning something in javaScript but map method is returning/generating new array.
 const fruits = ["banana", "apple", "peach", "mango", "grapes"];
 
-const value = fruits.map((val, i) => {
+console.log("Using ForEach Method.");
+
+const value = fruits.forEach((val, i, arr) => {
   let firstLetter = val[0].toUpperCase();
   let remainLetter = val.slice(1);
   console.log(i + 1, firstLetter + remainLetter);
+  console.log(arr);
   return firstLetter + remainLetter;
 });
 
 console.log(value);
+
+let trimValue = "\n \n \nUsing Map Method.";
+console.log(trimValue);
