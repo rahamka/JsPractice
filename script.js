5faf30d8f365d674d5ed8964f77c2c8355bc8124
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-03 13:35:18 +0500

    understand default parameters in JavaScript.

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-// What are Arguments in JavaScript?
-function argumentsFun(a, b, c, d, e, f) {
-  let sum = [];
-  for (let i = 0; i < arguments.length; i++) {
-    sum.push(arguments[i]);
-  }
-  sum = sum.reduce((prev, curr) => {
-    return prev + curr;
-  });
-  return sum;
+// Default Parameters in JavaScript.
+// how to Default Parameter in JavaScript to any Parameter of function.
+function multiply(a, b = 1) {
+  return a * b; // 6 * 1 // NaN => Not a Number.
 }
 
-console.log(argumentsFun(2, 3, 4, 3, 9, 1));
+console.log(multiply(3, ""));
+// Note: callBack function override the undefined by it's own value in JavaScript.
+console.log(true + 0); // true = 1;
+console.log(false + 0); // false = 0;
+console.log("" + 1); // false = 0;
 
-
-
-function add(a, b, c) {
-  console.log(arguments);
+// Setting Default value 6
+function randomNum(num = 6) {
+  return Math.floor(Math.random() * num) + 1; // 0.9
 }
 
-add(2, 3, 4);
+console.log(randomNum(10));
+
+// Default Parameter
+function DefaultFun(a, b = 5) {
+  // console.log(arguments);
+  return a * b;
+}
+
+console.log(DefaultFun(1));
