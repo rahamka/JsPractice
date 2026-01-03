ca1ef08a507a4538e481b9b4e956018cf6e75425
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-03 11:51:26 +0500

    changed the js.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const evenNumbers = [0, 2, 4, 6, 8, 10];
 
-const finalValue = evenNumbers.some((val, i) => {
-  return val % 2 === 1 ? console.log(i) : "null";
+const someMethod = evenNumbers.some((val) => {
+  return val > 4;
+});
+
+const finalValue = evenNumbers.every((val, i) => {
+  return val % 2 !== 0;
 });
 
 console.log(finalValue);
+console.log(someMethod);
