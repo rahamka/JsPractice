c634d16522d5fa16e96a117332b75fd941b436a8
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-03 11:38:17 +0500

    understand the some method in arrays.

--- script.js
+++ script.js
@@ -1,37 +1,7 @@
-// filter, map, reduce, forEach
+const evenNumbers = [0, 2, 4, 6, 8, 10];
 
-let arr = [1, 2, 3];
-// 6
-
-// using reduce method.
-const sum = arr.reduce((acc, curr) => {
-  return acc + curr;
-}, 1);
-
-console.log(sum);
-
-// using filter method
-const twoDivisible = arr.filter((val) => {
-  return val % 2 == 0;
+const finalValue = evenNumbers.some((val) => {
+  return val % 99 == 999;
 });
 
-console.log(twoDivisible);
-
-// using map method
-const mapMethod = arr.map((val) => {
-  return val / 2;
-});
-
-console.log(mapMethod);
-
-// using forEach method.
-//let arr = [1, 2, 3];
-arr.forEach((val) => {
-  console.log(val + 1);
-});
-
-// includes method => checks is includes value in the array.
-
-let nums = [1, 2, 3, 4, 5, 10, 23];
-let isResultAvailable = nums.includes(4);
-console.log(isResultAvailable);
+console.log(finalValue);
