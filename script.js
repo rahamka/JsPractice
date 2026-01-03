788287f748b57e9a52048754844a5082cda2b25e
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-03 12:58:55 +0500

    understand the arguments keyword in javascript.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // What are Arguments in JavaScript?
 function argumentsFun(a, b, c, d, e, f) {
-  let sum = 0;
+  let sum = [];
   for (let i = 0; i < arguments.length; i++) {
-    sum = sum + arguments[i];
+    sum.push(arguments[i]);
   }
+  sum = sum.reduce((prev, curr) => {
+    return prev + curr;
+  });
   return sum;
 }
 
 console.log(argumentsFun(2, 3, 4, 3, 9, 1));
+
+
+
+function add(a, b, c) {
+  console.log(arguments);
+}
+
+add(2, 3, 4);
