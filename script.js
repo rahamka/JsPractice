85049a9d1f6fbc3e7e470d6bff025b274e9779f5
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-03 11:50:49 +0500

    understand some and every method in js.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const evenNumbers = [0, 2, 4, 6, 8, 10];
 
-const finalValue = evenNumbers.some((val) => {
-  return val % 99 == 999;
+const finalValue = evenNumbers.some((val, i) => {
+  return val % 2 === 1 ? console.log(i) : "null";
 });
 
 console.log(finalValue);
