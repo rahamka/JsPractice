1c9535359fcaa5e2ec4919ba67270faa0148881d
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-04 14:27:28 +0500

    i understand the rest parameters in JavaScript.

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-// Spread Operator/Shallow Copy
-const nums = [1, 2, 3, 4];
-const nums2 = [5, 6, 7, 8];
+let nums = [1, 2, 3, 4];
 
-const jointNums = [...nums, ...nums2];
-console.log(jointNums);
-
-const student = {
-  Name: "Ahmad",
-  Class: "Xii",
-};
-
-const newClass = { ...student, City: "sagyoun" };
-console.log(newClass);
-
-// How both function works?
-function add() {
-  let arr = [...arguments[0]];
-  let result = arr.reduce(function (acc, curr) {
-    return acc + curr;
+function sum(num = [...nums]) {
+  let arr = [...num];
+  let result = arr.reduce((prev, curr) => {
+    return prev + curr;
   });
   return result;
 }
 
-console.log(add([2, 3, 4, 3, 5, 77]));
-
-// function 2
-function add() {
-  let arr = [arguments[0]]; // array inside array => [[2, 3, 4, 3, 5, 77]]
-  let result = arr.reduce(function (acc, curr) {
-    return acc + curr;
-  });
-  return result;
-}
-
-console.log(add([2, 3, 4, 3, 5, 77]));
+console.log(sum());
