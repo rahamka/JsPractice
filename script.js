ebd15d91d041c5bdb7b0c5c3b45bc063af19bc33
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-04 14:28:10 +0500

    updated the Js code for learning reset params.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let nums = [1, 2, 3, 4];
+// teach what's the difference in both functions?
 
-function sum(num = [...nums]) {
+let nums = [1, 2, 3, 4]; // 10
+
+function sum(a, b, ...num) {
+  // point: here a = 1, b = 2 & 3, 4 = ...num
   let arr = [...num];
   let result = arr.reduce((prev, curr) => {
     return prev + curr;
@@ -8,4 +11,25 @@ function sum(num = [...nums]) {
   return result;
 }
 
-console.log(sum());
+console.log(sum(4, 2, ...nums)); // why output is 10
+
+function sum(a, b, ...num) {
+  // point: here a = 1, b = 2 & 3, 4 = ...num
+  let arr = [...num];
+  let result = arr.reduce((prev, curr) => {
+    return prev + curr;
+  });
+  return result;
+}
+
+console.log(sum(4, 1, ...nums)); // why output is 10
+
+// test by self
+let arr = [1, 2, 3, 4];
+
+function sum2(a, b, c, ...num) {
+  console.log(arguments);
+  return [a, b, c, ...num].reduce((prev, curr) => prev + curr);
+}
+
+console.log(sum2(1, 2, 5, 7, 8, 9));
