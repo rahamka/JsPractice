0e6b99d1b7b583ecbe658bf2321a5d24029ae0bb
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-05 11:54:16 +0500

    understand the destructuring in Objects and Arrays.

--- script.js
+++ script.js
@@ -1,75 +1,104 @@
-// Destructuring in JavaScript.
+// // Destructuring in JavaScript.
 
-const colors = ["red", "green", "yellow", "blue", "orange", "pink", "skyBlue"];
+// const colors = ["red", "green", "yellow", "blue", "orange", "pink", "skyBlue"];
 
-// const [color1, color2, color3, color4, color5, color6, color7] = colors;
+// // const [color1, color2, color3, color4, color5, color6, color7] = colors;
 
-// How to get the third value by skipping other values
-// example: accessing yellow color
+// // How to get the third value by skipping other values
+// // example: accessing yellow color
 
-const [, , color3] = colors;
-console.log(color3);
+// const [, , color3] = colors;
+// console.log(color3);
 
-// we can set the value of array at any position using ,
-const yellowColor = [, , color3];
-console.log(yellowColor);
+// // we can set the value of array at any position using ,
+// const yellowColor = [, , color3];
+// console.log(yellowColor);
 
-// Destructuring on Objects
+// // Destructuring on Objects
+// const user = {
+//   Name: "Raham",
+//   Age: 20,
+//   address: {
+//     city: "Sagyoon",
+//     state: "Sindh",
+//   },
+// };
+
+// const {
+//   Name,
+//   Age,
+//   address: { city },
+//   address: { state },
+// } = user;
+
+// // console.log(city);
+// // Note: use destructuring method like i've used and get the value of city from address
+
+// // How to set the Name value to username variable using Destructuring.
+
+// const {
+//   Name: username,
+//   Age: userAge,
+//   address: { city: userCity }, // set the city value to userCity
+//   address: { state: userState }, // set the state value to userState
+// } = user;
+// console.log(userCity);
+
+// {
+//   // Multilevel Destructuring in Objects
+//   const {
+//     address: { city: userCity, state: userState },
+//   } = user;
+//   console.log("address = ", userCity, userState);
+// }
+
+// // Direct Accessing the values of array.
+// const { 3: blueColor } = colors;
+// console.log(blueColor);
+
+// // How to get the value from the Objects in Function?
+
+// function intro({ Age: UserAge, address: { city: UserCity } }) {
+//   console.log(UserAge);
+//   console.log(UserCity);
+// }
+
+// intro(user);
+
+// // How to get the value from the arrays in Objects?
+
+// function printColors(color) {
+//   const [, , , blueColor] = color;
+//   console.log(blueColor);
+// }
+
+// printColors(colors);
+
+let colors = ["red", "green", "blue"];
+const [, , blue] = colors;
+console.log(blue);
+
 const user = {
-  Name: "Raham",
-  Age: 20,
+  userName: "ali",
+  userAge: 20,
   address: {
-    city: "Sagyoon",
-    state: "Sindh",
+    userCity: "Sagyoon",
+    userState: "Sindh",
   },
 };
 
 const {
-  Name,
-  Age,
-  address: { city },
-  address: { state },
+  userName: Name,
+  userAge: Age,
+  address: { userState: State, userCity: City },
 } = user;
+console.log(City);
 
-// console.log(city);
-// Note: use destructuring method like i've used and get the value of city from address
-
-// How to set the Name value to username variable using Destructuring.
-
-const {
-  Name: username,
-  Age: userAge,
-  address: { city: userCity }, // set the city value to userCity
-  address: { state: userState }, // set the state value to userState
-} = user;
-console.log(userCity);
-
-{
-  // Multilevel Destructuring in Objects
-  const {
-    address: { city: userCity, state: userState },
+function userDataFun(user) {
+  let {
+    address: { userState },
   } = user;
-  console.log("address = ", userCity, userState);
+  console.log(userState);
 }
 
-// Direct Accessing the values of array.
-const { 3: blueColor } = colors;
-console.log(blueColor);
-
-// How to get the value from the Objects in Function?
-
-function intro({ Age: UserAge, address: { city: UserCity } }) {
-  console.log(UserAge);
-  console.log(UserCity);
-}
-
-intro(user);
-
-// How to get the value from the arrays in Objects?
-
-function printColors(color) {
-  const [, , , blueColor] = color;
-  console.log(blueColor);
-}
-
-printColors(colors);
+userDataFun(user);
