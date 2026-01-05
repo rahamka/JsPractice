662256f9cdb4882fbd6dddd28fa7609ab4a7f14f
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-05 10:08:29 +0500

    updated the JavaScript.

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-// teach what's the difference in both functions?
-
-let nums = [1, 2, 3, 4]; // 10
-
-function sum(a, b, ...num) {
-  // point: here a = 1, b = 2 & 3, 4 = ...num
-  let arr = [...num];
-  let result = arr.reduce((prev, curr) => {
-    return prev + curr;
-  });
-  return result;
-}
-
-console.log(sum(4, 2, ...nums)); // why output is 10
-
-function sum(a, b, ...num) {
-  // point: here a = 1, b = 2 & 3, 4 = ...num
-  let arr = [...num];
-  let result = arr.reduce((prev, curr) => {
-    return prev + curr;
-  });
-  return result;
-}
-
-console.log(sum(4, 1, ...nums)); // why output is 10
-
-// test by self
+// spread operator
 let arr = [1, 2, 3, 4];
+let shallowCopy = [...arr];
+console.log(shallowCopy);
 
-function sum2(a, b, c, ...num) {
-  console.log(arguments);
-  return [a, b, c, ...num].reduce((prev, curr) => prev + curr);
-}
+// function definition
 
-console.log(sum2(1, 2, 5, 7, 8, 9));
+// what is callStack in JavaScript?
+// In JavaScript when we're creating the function then we're
+// calling that function and the process in which function is
+// executing that process is called callStack in JavaScript.
+
+const sum = (val1, val2) => {
+  return val1 + val2;
+};
+
+arr.forEach((val) => {
+  console.log(val + 1);
+});
+
+// Implicit return
+
+const newArr = [1, 2, 3, 4];
+const result = newArr.reduce((prev, curr) => prev + curr);
+console.log("Implicit = ", result);
+
+const exResult = newArr.reduce((prev, curr) => {
+  return prev + curr;
+});
+
+console.log("ExResult = ", exResult);
+
+console.log("Final Result = ", result + exResult);
