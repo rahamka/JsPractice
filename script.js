4a4d7b56d9d856125a54568cee695abe1a139e11
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-06 14:04:24 +0500

    understand the selectors in js.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let el1 = document.body.children[4].children[2].children[4];
-el1.src =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9PSg6yObKaXf3iS5D_--xBsONlxe7GP_XjA&s";
+// Selecting the Elements in JavaScript.
+let h1 = document.getElementById("h1");
+console.log(h1);
 
-let el2 = document.body.children[4].children[3].children[1];
-el2.src =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlZ2vhLzIdtwpwXw90ZuWhngB-MdFsjD64CQ&s";
+let img1 = document.getElementById("img1");
+console.log(img1);
+
+let img11 = document.querySelector("#img1");
+console.log(img11);
+
+let img = document.getElementsByClassName("img");
+console.log(img);
