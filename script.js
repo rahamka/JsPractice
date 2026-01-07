c2693e6b6c0ef7f6c34261cccd062ec33c6da1e9
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-07 09:30:56 +0500

    reviewed the complete Js.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-// Selecting the Elements in JavaScript.
-let h1 = document.getElementById("h1");
-console.log(h1);
+// How to interact with DOM?
+// What is BOM?
+// In JavaScript BOM is like a DOM BOM means [Browser Object Model] means representation of the Browser
 
-let img1 = document.getElementById("img1");
-console.log(img1);
+function sum(a = 1, b, c) {
+  console.log(a);
+  return a + b + c;
+}
 
-let img11 = document.querySelector("#img1");
-console.log(img11);
+console.log(sum(2, 2)); // NaN ?
+// why answer of this line of code is NaN? while i've declared the by default value to a parameter?
 
-let img = document.getElementsByClassName("img");
-console.log(img);
+function spreadOperator(a, b, ...c) {
+  return a + b + c.reduce((prev, curr) => prev + curr);
+}
+
+console.log(spreadOperator(1, 2, 3, 4, 5, 6));
+
+//
+let colors = ["red", "blue", "green"];
+const [red, blue, green] = colors;
+console.log(red);
+
+//
+let student = {
+  Name: "Ahmad",
+  Class: "Ix",
+  Age: 18,
+  isGraduate: false,
+};
+
+const { Name: userName, Class: userClass, Age: userAge, isGraduate } = student;
+console.log(userAge);
