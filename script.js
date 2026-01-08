8b0fe605b8a003fa8ff6e0159b862c114a529c38
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-08 17:43:26 +0500

    i revised all the JavaScript which i've learned.

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-let anchors = document.getElementsByTagName("a");
-let value = [...anchors];
-value.forEach((val) => {
-  val.style = `color: blue;
-  background-color: yellow;
-  border: 2px solid green;
-  `;
-  console.log(val.nodeType);
-});
+let element = document.getElementById("heading");
+let allStyles = window.getComputedStyle(element);
+// console.log(allStyles);
 
-let heading = document.body.childNodes;
+// Object Copy
+let student = {
+  Name: "Ali",
+  Class: "Ix",
+  isGraduate: false,
+  Age: 19,
+};
 
-heading.forEach((val) => {
-  console.log(val, val.nodeType);
+let newStudent = { ...student };
+// console.log(newStudent);
+
+// Default Parameter
+
+function sum(a, b = 4) {
+  return a + b;
+}
+
+console.log("sum = ", sum(10, 5));
+
+// Rest Parameter.
+
+function resetFun(a, b, ...c) {
+  return [a, b, c]; // [1, 2, undefined]
+}
+
+console.log(resetFun(1, 2, 5, 6, 7, 10, 11));
+
+// Destructuring and skip method for getting any value from the array.
+let colors = ["red", "blue", "pink", "yellow", "skyblue", "green"];
+
+const [, , pink, , ,] = colors;
+
+console.log(pink); // blue
+
+// nums array
+let nums = [2, 3, 4, 5, 10, 20]; // even numbers
+let result = [];
+let divisible = nums.some((val) => {
+  return val % 2 == 0 ? result.push(val) : false;
 });
+console.log(result);
