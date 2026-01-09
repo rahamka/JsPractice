2573042be03efb6a79d22748d85d4e5c392ceb34
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-09 13:06:51 +0500

    updated the javascript.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+// let container = document.getElementById("container");
+// let card = document.getElementById("card");
+
+// for (let i = 1; i <= 100; i++) {
+//   let cardCopy = card.cloneNode(true);
+//   cardCopy.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+//   cardCopy.innerHTML = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+//   container.appendChild(cardCopy);
+// }
+
+// card.remove();
+
 let container = document.getElementById("container");
-let card = document.getElementById("card");
 
 for (let i = 1; i <= 100; i++) {
-  let cardCopy = card.cloneNode(true);
-  cardCopy.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-  cardCopy.innerHTML = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-  container.appendChild(cardCopy);
+  let el = document.createElement("p");
+  el.innerText = `${i}`;
+  el.id = `para-${i}`;
+  el.className = `para-${i}`;
+  container.appendChild(el);
+  el.style.color = "black";
 }
-
-card.remove();
