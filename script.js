cee3bbcaea8130fcd9c7266ea725991a243eec25
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-09 13:23:42 +0500

    understand how to add numbers in the container if there is another element.

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let container = document.getElementById("container");
 let card = document.getElementById("card");
 
 for (let i = 1; i <= 100; i++) {
+  let num = document.createElement("p");
+  num.innerText = i;
   let cardCopy = card.cloneNode(true);
+  let newContainer = document.createElement("div");
+  newContainer.id = "newContainer";
+  newContainer.prepend(num);
   cardCopy.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-  cardCopy.innerHTML = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-  container.appendChild(cardCopy);
+  newContainer.appendChild(cardCopy);
+  container.appendChild(newContainer);
 }
 
 card.remove();
