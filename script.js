b7329ceddb233726950425e1d298b61446c4d59e
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-10 13:18:41 +0500

    updated all files including html & Js.

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
-let container = document.getElementById("container");
-let card = document.getElementById("card");
+let username = document.getElementById("username");
 
-for (let i = 1; i <= 1; i++) {
-  let newContainer = document.createElement("div");
-  let num = document.createElement("p");
-  num.innerText = i;
-  newContainer.prepend(num);
-  let cardCopy = card.cloneNode(true);
-  cardCopy.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-  cardCopy.id = `id-${i}`;
-  newContainer.appendChild(cardCopy);
-  newContainer.classList.add("newContainer");
-  container.appendChild(newContainer);
-}
-
-let el1 = document.getElementById("id-1");
-let clickCounter = 1;
-el1.addEventListener("click", () => {
-  clickCounter++;
-  let newContainer = document.createElement("div");
-  let num = document.createElement("p");
-  num.innerText = clickCounter;
-  newContainer.prepend(num);
-  let cardCopy = card.cloneNode(true);
-  cardCopy.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${clickCounter}.png`;
-  cardCopy.id = `id-${clickCounter}`;
-  newContainer.appendChild(cardCopy);
-  newContainer.classList.add("newContainer");
-  container.appendChild(newContainer);
+username.addEventListener("input", (val) => {
+  console.log(val.data);
 });
-
-function sayHii() {
-  console.log("Hii");
-}
-
-setTimeout(sayHii, 2000);
