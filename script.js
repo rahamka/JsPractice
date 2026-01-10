dea19a5244a0792356d69209b5b8be92a48dbb41
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-10 10:55:25 +0500

    understand the JavaScrip code.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-let container = document.getElementById("container");
-let card = document.getElementById("card");
+const person = {
+  firstName: "Bruce",
+  lastName: "Wayne",
+};
 
-for (let i = 1; i <= 100; i++) {
-  let num = document.createElement("p");
-  num.innerText = i;
-  let cardCopy = card.cloneNode(true);
-  let newContainer = document.createElement("div");
-  newContainer.id = "newContainer";
-  newContainer.prepend(num);
-  cardCopy.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-  newContainer.appendChild(cardCopy);
-  container.appendChild(newContainer);
-}
-
-card.remove();
+console.log(person);
